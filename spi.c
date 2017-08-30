9b4d30850572b7d3144295764d7bc3b6958ce632
Dmitry Gerasimov <di.gerasimov@gmail.com>
2017-08-30 23:43:00 +0300

    Move defines to header

--- spi.c
+++ spi.c
@@ -3,13 +3,6 @@
 #include <avr/cpufunc.h>
 #include "spi.h"
 
-#define SPI_PORT PORTB
-#define SPI_PIN PINB
-#define SPI_DDR DDRB
-#define SPI_MOSI PB5
-#define SPI_MISO PB6
-#define SPI_SCLK PB7
-
 void spi_init(void)
 {
     SPI_DDR |= _BV(SPI_MOSI) | _BV(SPI_SCLK);
--- spi.h
+++ spi.h
@@ -3,6 +3,13 @@
 
 #include <stdint.h>
 
+#define SPI_PORT PORTB
+#define SPI_PIN PINB
+#define SPI_DDR DDRB
+#define SPI_MOSI PB5
+#define SPI_MISO PB6
+#define SPI_SCLK PB7
+
 void spi_init(void);
 uint8_t spi_transfer(uint8_t data);
 
