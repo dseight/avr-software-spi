1929520171550a9adf6713058d4f1a728ae0fa33
Dmitry Gerasimov <di.gerasimov@gmail.com>
2017-08-31 21:50:26 +0300

    Add functions description in header

--- spi.h
+++ spi.h
@@ -10,7 +10,11 @@
 #define SPI_MISO PB6
 #define SPI_SCLK PB7
 
+/* Init SPI on pins specified above */
 void spi_init(void);
+
+/* Transfer data over SPI (SPI Mode 0 with MSB first)
+ * Returns recieved data */
 uint8_t spi_transfer(uint8_t data);
 
 #endif
